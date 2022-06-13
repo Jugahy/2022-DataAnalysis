f71fe6b4a7c2c822ec3700e75406616f559bafea
jugahy <jugahy1205@gmail.com>
2022-06-13 20:36:58 +0900

    Initial Commit

--- 2022년_전라북도_공공데이터_활용_창업경진대회/Analysis/k-평균알고리즘.py
+++ 2022년_전라북도_공공데이터_활용_창업경진대회/Analysis/k-평균알고리즘.py
@@ -13,18 +13,57 @@ data = [[1, 2], [3, 5], [3, 2], [2, 1], [4, 2], [2, 3], [4, 4], [4, 5], [5, 5], 
 
 # 군집 수 선택
 k = [0, 4]
-one_mid = 0
-two_mid = 0
 
-new_one_mid = 0
-new_two_mid = 0
-# 연산
-for i in range(100):
+# 새로운 평균값
+one_mid_x = 0
+one_mid_y = 0
+two_mid_x = 0
+two_mid_y = 0
+
+# 처음 주어진 좌표에서 평균값 구하는 연산
+for i in range(len(data)):
+
+    # 군집 나누는 기준
+    one_distance.append(math.sqrt((data[i][0] - data[k[0]][0]) ** 2 + (data[i][1] - data[k[0]][1]) ** 2))
+    two_distance.append(math.sqrt((data[i][0] - data[k[1]][0]) ** 2 + (data[i][1] - data[k[1]][1]) ** 2))
+
+    # 군집 나누기
+    if one_distance[i] > two_distance[i]:
+        group.append(2)
+    else:
+        group.append(1)
+
+# 현재 군집 기억해주는 군집
+remember_group = group
+
+# group 뽑았으니 리스트 비워주기
+one_distance = []
+two_distance = []
+
+# 계속해서 group 유지하게 만드는 연산
+for w in range(3):
+
+    # 새로운 평균값 구하기
+    for i in range(len(group)):
+        if group[i] == 1:
+            one_mid_x += data[i][0]
+            one_mid_y += data[i][1]
+        else:
+            two_mid_x += data[i][0]
+            two_mid_y += data[i][1]
+
+    new_one_mid_x = one_mid_x/group.count(1)
+    new_one_mid_y = one_mid_y/group.count(1)
+    new_two_mid_x = two_mid_x/group.count(2)
+    new_two_mid_y = two_mid_y/group.count(2)
+
+    # group 다 썻으니 초기화
+    group = []
+
     for i in range(len(data)):
-
         # 군집 나누는 기준
-        one_distance.append(math.sqrt((data[i][0] - data[k[0]][0]) ** 2 + (data[i][1] - data[k[0]][1]) ** 2))
-        two_distance.append(math.sqrt((data[i][0] - data[k[1]][0]) ** 2 + (data[i][1] - data[k[1]][1]) ** 2))
+        one_distance.append(math.sqrt((data[i][0] - new_one_mid_x) ** 2 + (data[i][1] - new_one_mid_y) ** 2))
+        two_distance.append(math.sqrt((data[i][0] - new_two_mid_x) ** 2 + (data[i][1] - new_two_mid_y) ** 2))
 
         # 군집 나누기
         if one_distance[i] > two_distance[i]:
@@ -34,25 +73,14 @@ for i in range(100):
 
     # 군집의 변화가 없다면 멈춤
     if remember_group == group:
+        print("호홍")
         break
 
     # 현재 군집 기억해주는 군집
-    remember_group = group
+    else:
+        remember_group = group
 
-    # 새로운 평균값 구하기
-    for q in range(len(group)):
-        if group[q] == 1:
-            one_mid.append(data[q][0])
-        else:
-            two_mid.append(data[q][1])
-
-    new_one_mid = sum(one_mid)/len(one_mid)
-    new_two_mid = sum(two_mid)/len(two_mid)
-
-print(new_one_mid)
-print(new_two_mid)
-
-
+print(group)
 
 
 # for i in range(len(data)):
