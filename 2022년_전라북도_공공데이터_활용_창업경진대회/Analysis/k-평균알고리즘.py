640de4fd1db305aa53105650dc939d9d93c1c75d
jugahy <jugahy1205@gmail.com>
2022-06-13 20:53:34 +0900

    Initial Commit

--- 2022년_전라북도_공공데이터_활용_창업경진대회/Analysis/k-평균알고리즘.py
+++ 2022년_전라북도_공공데이터_활용_창업경진대회/Analysis/k-평균알고리즘.py
@@ -41,7 +41,7 @@ one_distance = []
 two_distance = []
 
 # 계속해서 group 유지하게 만드는 연산
-for w in range(3):
+for w in range(100):
 
     # 새로운 평균값 구하기
     for i in range(len(group)):
@@ -52,10 +52,18 @@ for w in range(3):
             two_mid_x += data[i][0]
             two_mid_y += data[i][1]
 
-    new_one_mid_x = one_mid_x/group.count(1)
-    new_one_mid_y = one_mid_y/group.count(1)
-    new_two_mid_x = two_mid_x/group.count(2)
-    new_two_mid_y = two_mid_y/group.count(2)
+    new_one_mid_x = one_mid_x / group.count(1)
+    new_one_mid_y = one_mid_y / group.count(1)
+    new_two_mid_x = two_mid_x / group.count(2)
+    new_two_mid_y = two_mid_y / group.count(2)
+
+    # 중첩되는 것이니 초기화
+    one_mid_x = 0
+    one_mid_y = 0
+    two_mid_x = 0
+    two_mid_y = 0
+    one_distance = []
+    two_distance = []
 
     # group 다 썻으니 초기화
     group = []
@@ -73,15 +81,18 @@ for w in range(3):
 
     # 군집의 변화가 없다면 멈춤
     if remember_group == group:
-        print("호홍")
+        print("전 그룹과 현재 그룹이 같으므로 프로그램 종료")
+        print("군집 1과의 거리 :", one_distance)
+        print("군집 2와의 거리 :", two_distance)
+        print("소속될 군집 :", group)
+        print("군집1의 x좌표 : %.2f, 군집1의 y좌표 : %.2f" % (new_one_mid_x, new_one_mid_y))
+        print("군집2의 x좌표 : %.2f, 군집2의 y좌표 : %.2f" % (new_two_mid_x, new_two_mid_y))
+
         break
 
     # 현재 군집 기억해주는 군집
     else:
         remember_group = group
-
-print(group)
-
 
 # for i in range(len(data)):
 #     plt.scatter(data[i][0], data[i][1], color="black")
