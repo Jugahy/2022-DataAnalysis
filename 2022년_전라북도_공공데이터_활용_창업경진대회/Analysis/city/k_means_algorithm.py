84de9e539c8c4e7c7520d7231c38a9e6fc958d34
jugahy <jugahy1205@gmail.com>
2022-06-19 00:52:24 +0900

    Initial Commit

--- 2022년_전라북도_공공데이터_활용_창업경진대회/Analysis/city/k_means_algorithm.py
+++ 2022년_전라북도_공공데이터_활용_창업경진대회/Analysis/city/k_means_algorithm.py
@@ -10,6 +10,7 @@ data = [[1, 2], [3, 5], [3, 2], [2, 1], [4, 2], [2, 3], [4, 4], [4, 5], [5, 5], 
 k = [0, 4, 6]
 
 
+
 # 데이터 형식 : [[위도1,경도1], [위도2,경도2], ...[위도n,경도n]]
 # k 형식 : data의 값 중 원하는 좌표의 인덱스 [0,8]
 def put(data, k):
@@ -20,6 +21,7 @@ def put(data, k):
     print(mid_list)
 
 
+# data의 좌표를 이용할 때 군집 나누는 함수
 def cluster():
     import math
 
@@ -43,9 +45,64 @@ def cluster():
     return remember_group
 
 
+# 새로운 평균값 만드는 함수
 def mid():
-    for i in range(len(data)):
-        for j in range(len(k)):
-            if remember_group[i] ==
+    global new_mid
+    all_mid = []
+    new_mid = []
 
-cluster()
+    all_mid.append([])
+    all_mid.append([])
+
+    for i in range(len(k)):
+        new_mid.append([])
+
+        for j in [l for l, x in enumerate(remember_group) if x == k[i]]:
+            all_mid[0].append(data[j][0])
+            all_mid[1].append(data[j][1])
+
+        new_mid[i].append(sum(all_mid[0]) / remember_group.count(k[i]))
+        new_mid[i].append(sum(all_mid[1]) / remember_group.count(k[i]))
+
+        all_mid[0] = []
+        all_mid[1] = []
+    return new_mid
+
+
+# 새로운 평균값의 좌표를 이용할 때 군집 나누는 함수
+def k_cluster():
+    import math
+    global group, reremember_group
+
+    distance = []
+    group = []
+    reremember_group = []
+
+    for i in range(len(data)):
+        distance.append([])
+
+        for j in new_mid:
+            distance[i].append(math.sqrt((data[i][0] - j[0]) ** 2 + (data[i][1] - j[1]) ** 2))
+
+    for i in range(len(data)):
+        group.append(k[distance[i].index(min(distance[i]))])
+
+    if remember_group == group:
+        print(new_mid)
+        print(group)
+
+
+
+    reremember_group = group
+
+    return group, reremember_group
+
+
+def main():
+    cluster()
+    mid()
+    k_cluster()
+    if remember_group == group:
+        print(new_mid)
+        print()
+        break
