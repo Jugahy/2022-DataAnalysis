6f4ca1c8e772d4cdde779faf2673b0104ba5ed6c
jugahy <jugahy1205@gmail.com>
2022-07-06 22:57:08 +0700

    Initial Commit

---
+++ 2022년_전라북도_공공데이터_활용_창업경진대회/Analysis/scikit-learn/Elbow-Method.py
@@ -0,0 +1,19 @@
+from sklearn.cluster import KMeans
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+df = pd.read_csv("C:/Users/jugah/PycharmProjects/2022-DataAnalysis/Data/공공데이터포털/전북_통합.csv", encoding="euc-kr")
+data = {"x" : df["경도"], "y" : df["위도"]}
+points = pd.DataFrame(data)
+
+inertia = []
+for i in range(1, 100):
+    kmeans_plus = KMeans(n_clusters=i, init='k-means++')
+    kmeans_plus.fit(points)
+    inertia.append(kmeans_plus.inertia_)
+
+plt.plot(range(1, 100), inertia, marker='o')
+plt.xlabel('Number of clusters')
+plt.ylabel('Inertia')
+plt.show()
---
+++ 2022년_전라북도_공공데이터_활용_창업경진대회/Analysis/scikit-learn/Silhouette-Method.py
@@ -0,0 +1,21 @@
+from sklearn.cluster import KMeans
+from sklearn.metrics import silhouette_samples
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+df = pd.read_csv("C:/Users/jugah/PycharmProjects/2022-DataAnalysis/Data/공공데이터포털/전북_통합.csv", encoding="euc-kr")
+data = {"x" : df["경도"], "y" : df["위도"]}
+points = pd.DataFrame(data)
+
+silhouette_vals = []
+for i in range(2, 300):
+    kmeans_plus = KMeans(n_clusters=i, init='k-means++')
+    pred = kmeans_plus.fit_predict(points)
+    silhouette_vals.append(np.mean(silhouette_samples(points, pred, metric='euclidean')))
+
+plt.plot(range(2, 100), silhouette_vals, marker='o')
+plt.xlabel('Number of clusters')
+plt.ylabel('Silhouette')
+plt.show()
