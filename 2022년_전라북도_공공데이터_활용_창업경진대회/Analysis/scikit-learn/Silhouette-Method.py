ce202a98f679c1e6c39e2eac0f8942812176dd1d
jugahy <jugahy1205@gmail.com>
2022-07-12 22:18:49 -0300

    Initial Commit

--- 2022년_전라북도_공공데이터_활용_창업경진대회/Analysis/scikit-learn/Silhouette-Method.py
+++ 2022년_전라북도_공공데이터_활용_창업경진대회/Analysis/scikit-learn/Silhouette-Method.py
@@ -10,12 +10,12 @@ data = {"x" : df["경도"], "y" : df["위도"]}
 points = pd.DataFrame(data)
 
 silhouette_vals = []
-for i in range(2, 300):
+for i in range(2, 200):
     kmeans_plus = KMeans(n_clusters=i, init='k-means++')
     pred = kmeans_plus.fit_predict(points)
     silhouette_vals.append(np.mean(silhouette_samples(points, pred, metric='euclidean')))
 
-plt.plot(range(2, 100), silhouette_vals, marker='o')
+plt.plot(range(2, 200), silhouette_vals, marker='o')
 plt.xlabel('Number of clusters')
 plt.ylabel('Silhouette')
 plt.show()
